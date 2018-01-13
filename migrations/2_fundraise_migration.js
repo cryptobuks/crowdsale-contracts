9a440308375ab2a0886158d7cdda64dcdfc6ea31
Henry Fisher <henry.wfisher@gmail.com>
2018-01-13 12:41:51 -0800

    Should be good to deploy to rinkeby, need to test

---
+++ migrations/2_fundraise_migration.js
@@ -0,0 +1,7 @@
+var Fundraise = artifacts.require("./Fundraise.sol");
+
+module.exports = function(deployer) {
+	account1 = address;
+	account2 = address;
+  	deployer.deploy(Fundraise, account1, account2);
+};
--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      network_id: 4,
+      host: '127.0.0.1',
+      port: 8545,
+      gas: 4000000
+    }
+  }
 };
