24999cb129806ec9d9916ae965d9336800d20d39
Alex Altair <alexander.altair@gmail.com>
2018-02-08 20:29:06 -0800

    Remove opening and closing functionality; fill out unit tests.

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -3,16 +3,12 @@ pragma solidity ^0.4.18;
 contract Fundraiser {
 
   /* State */
-
   address signer1;
   address signer2;
-  bool public accept; // are contributions accepted
 
   enum Action {
     None,
-    Withdraw,
-    Close,
-    Open
+    Withdraw
   }
   
   struct Proposal {
@@ -27,7 +23,6 @@ contract Fundraiser {
   /* Constructor, choose signers. Those cannot be changed */
   function Fundraiser(address init_signer1,
                       address init_signer2) public {
-    accept = false; // must call Open first
     signer1 = init_signer1;
     signer2 = init_signer2;
     signer1_proposal.action = Action.None;
@@ -35,26 +30,22 @@ contract Fundraiser {
   }
 
   /* Entry point for contributors */
-
-  event Deposit (
-                 bytes20 flamingo_pk_hash,
-                 uint amount
-                 );
+  event Deposit (bytes20 flamingo_pk_hash, uint amount);
 
   function Contribute(bytes24 flamingo_pkh_and_chksum) public payable {
     // Don't accept contributions if fundraiser closed
-    require(accept);
     bytes20 flamingo_pk_hash = bytes20(flamingo_pkh_and_chksum);
+
     /* shift left 20 bytes to extract checksum */
-    bytes4 expected_chksum = bytes4(flamingo_pkh_and_chksum << 160);
+    bytes4 expected_chksum = bytes4(flamingo_pkh_and_chksum << 8*20);
     bytes4 chksum = bytes4(sha256(sha256(flamingo_pk_hash)));
+
     /* revert transaction if the checksum cannot be verified */
     require(chksum == expected_chksum);
     Deposit(flamingo_pk_hash, msg.value);
   }
 
   /* Entry points for signers */
-
   function Withdraw(address proposed_destination,
                     uint256 proposed_amount) public {
     /* check amount */
@@ -76,34 +67,6 @@ contract Fundraiser {
     MaybePerformWithdraw();
   }
 
-  function Close(address proposed_destination) public {
-    /* check that only one of the signers is requesting */
-    require(msg.sender == signer1 || msg.sender == signer2);
-    /* update action */
-    if (msg.sender == signer1) {
-      signer1_proposal.action = Action.Close;
-      signer1_proposal.destination = proposed_destination;
-    } else if (msg.sender == signer2) {
-      signer2_proposal.action = Action.Close;
-      signer2_proposal.destination = proposed_destination;
-    } else { assert(false); }
-    /* perform action */
-    MaybePerformClose();
-  }
-
-  function Open() public {
-    /* check that only one of the signers is requesting */
-    require(msg.sender == signer1 || msg.sender == signer2);
-    /* update action */
-    if (msg.sender == signer1) {
-      signer1_proposal.action = Action.Open;
-    } else if (msg.sender == signer2) {
-      signer2_proposal.action = Action.Open;
-    } else { assert(false); }
-    /* perform action */
-    MaybePerformOpen();
-  }
-
   function MaybePerformWithdraw() internal {
     if (signer1_proposal.action == Action.Withdraw
         && signer2_proposal.action == Action.Withdraw
@@ -112,22 +75,6 @@ contract Fundraiser {
       signer1_proposal.action = Action.None;
       signer2_proposal.action = Action.None;
       signer1_proposal.destination.transfer(signer1_proposal.amount);
-    }
-  }
-
-  function MaybePerformClose() internal {
-    if (signer1_proposal.action == Action.Close
-        && signer2_proposal.action == Action.Close
-        && signer1_proposal.destination == signer2_proposal.destination) {
-      accept = false;
-      signer1_proposal.destination.transfer(this.balance);
-    }
-  }
-
-  function MaybePerformOpen() internal {
-    if (signer1_proposal.action == Action.Open
-        && signer2_proposal.action == Action.Open) {
-      accept = true;
     }
   }
 }
--- test/fundraiser.js
+++ test/fundraiser.js
@@ -25,188 +25,121 @@ contract('Fundraiser', function(accounts) {
         }
     });
 
-    it("is initialized closed", async function() {
-        const accept = await fundr.accept.call();
-        assert(!accept);
-    });
+    describe("when trying to contribute", function() {
+        it("allow if it's a signer", async function() {
+            const balance_before = await web3.eth.getBalance(fundr.address);
+            const amount_sent = 1
 
-    describe('when closed', function() {
-        describe("when trying to contribute", function() {
-            it("reverts if it's a signer", async function() {
-                try {
-                    await fundr.Contribute(1234, {from: signer1_address});
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
+            await fundr.Contribute(
+                // This hex is the concatenation of a fake public key and its double hash.
+                web3.toHex("0x0000000000000000000000000000000000000000f6eab7b9"),
+                {from: signer1_address, value: amount_sent}
+            );
 
-            it("reverts if it's not a signer", async function() {
-                try {
-                    await fundr.Contribute(1234, {from: contributer_address});
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
+            const balance_after = await web3.eth.getBalance(fundr.address)
+            assert(balance_after.equals(balance_before.plus(amount_sent)));
         });
 
-        describe("when trying to withdraw", function() {
-            it("allows one signer to propose but not withdraw", async function() {
-                // put some money in the account so you withdraw it
-                // fundr.Withdraw(dest, amount)
-            });
+        it("allow if it's not a signer", async function() {
+            const balance_before = await web3.eth.getBalance(fundr.address);
+            const amount_sent = 1;
 
-            it("allows both signers to withdraw", async function() {
-                // actually check that money went out!
-            });
+            await fundr.Contribute(
+                // This hex is the concatenation of a fake public key and its double hash.
+                web3.toHex("0x0000000000000000000000000000000000000000f6eab7b9"),
+                {from: contributer_address, value: amount_sent}
+            );
 
-            it("does not allow a non-signer to propose withdrawal", async function() {
-            });
-
-            it("does not allow the signers to withdraw more than is in the account", async function() {
-            });
+            const balance_after = await web3.eth.getBalance(fundr.address);
+            assert(balance_after.equals(balance_before.plus(amount_sent)));
         });
 
-        describe('when trying to open', function() {
-            it("reverts when not one of the signers", async function() {
-                try {
-                    await fundr.Open({from: contributer_address});
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
-
-            it("allows one of the signers to propose but does not yet open", async function() {
-                await fundr.Open({from: signer1_address});
-                const accept = await fundr.accept.call();
-                assert.equal(accept, false);
-            });
-
-            it("opens after both signers propose", async function() {
-                await fundr.Open({from: signer1_address});
-                await fundr.Open({from: signer2_address});
-                const accept = await fundr.accept.call();
-                assert.equal(accept, true);
-            });
-
-            it("opens after both signers sign in the other order", async function() {
-                await fundr.Open({from: signer2_address});
-                await fundr.Open({from: signer1_address});
-                const accept = await fundr.accept.call();
-                assert.equal(accept, true);
-            });
+        it("revert if pk checksum is wrong", async function() {
+            try {
+                await fundr.Contribute(
+                    // This hex of all zeros is wrong because it needs its own double hash concatenated on the end.
+                    web3.toHex("0x000000000000000000000000000000000000000000000000"),
+                    {from: contributer_address}
+                );
+                assert.fail(null, null, "Contract should have raised.");
+            } catch(error) {
+                assert.equal(error.message, "VM Exception while processing transaction: revert");
+            }
         });
     });
 
-    describe('when open', function() {
+    describe("when trying to withdraw", function() {
         beforeEach(async function () {
-            await fundr.Open({from: signer1_address});
-            await fundr.Open({from: signer2_address});
+            await fundr.Contribute(
+                // This hex is the concatenation of a fake public key and its double hash.
+                web3.toHex("0x0000000000000000000000000000000000000000f6eab7b9"),
+                {from: contributer_address, value: 1}
+            );
         });
 
-        describe("when trying to contribute", function() {
-            it("allow if it's a signer", async function() {
-            });
+        it("allows one signer to propose but not withdraw", async function() {
+            const balance_before = await web3.eth.getBalance(fundr.address);
+            await fundr.Withdraw(destination_address, 1, {from: signer1_address})
+            const balance_after = await web3.eth.getBalance(fundr.address)
 
-            it("allow if it's not a signer", async function() {
-                // actually check that the money went in!
-                // await fundr.Contribute(0x212121210000000000000000000000000000000000000001, {from: contributer_address});
-                // assert.fail()
-            });
-
-            it("revert if the pk_hash is wrong", async function() {
-            });
-
-            it("revert if pk checksum is wrong", async function() {
-            });
+            assert(balance_after.equals(balance_before));
         });
 
-        describe("when trying to withdraw", function() {
-            it("allows one signer to propose but not withdraw", async function() {
-                // Must put ETH in account first
-                // await fundr.Withdraw(destination_address, 1, {from: signer1_address})
-            });
+        it("allows both signers to withdraw", async function() {
+            const amount_withdrawn = 1;
 
-            it("allows both signers to withdraw", async function() {
-                // Must put ETH in account first
-                // await fundr.Withdraw(destination_address, 1, {from: signer1_address})
-                // await fundr.Withdraw(destination_address, 1, {from: signer2_address})
-            });
+            const balance_before = await web3.eth.getBalance(fundr.address);
+            const dest_balance_before = await web3.eth.getBalance(destination_address);
 
-            it("does not allow a non-signer to propose withdrawal", async function() {
-                // Must put ETH in account first
-                try {
-                    await fundr.Withdraw(destination_address, 1, {from: contributer_address})
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
+            await fundr.Withdraw(destination_address, amount_withdrawn, {from: signer1_address});
+            await fundr.Withdraw(destination_address, amount_withdrawn, {from: signer2_address});
 
-            it("does not allow the signers to withdraw more than is in the account", async function() {
-                const contract_balance_before = await web3.eth.getBalance(fundr.address)
-                const withdraw_amount = contract_balance_before.plus(1)
-                try {
-                    await fundr.Withdraw(destination_address, withdraw_amount, {from: signer1_address})
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
+            const balance_after = await web3.eth.getBalance(fundr.address);
+            const dest_balance_after = await web3.eth.getBalance(destination_address);
+
+            // Then make sure that amount was withdrawn
+            // plus and equals methods are because these are BigNumbers
+            assert(balance_after.plus(amount_withdrawn).equals(balance_before));
+            assert(dest_balance_before.plus(amount_withdrawn).equals(dest_balance_after));
         });
 
-        describe('when trying to close', function() {
-            it("reverts when not one of the signers", async function() {
-                try {
-                    await fundr.Close(destination_address, {from: contributer_address})
-                    assert.fail(null, null, "Contract should have raised.");
-                } catch(error) {
-                    assert.equal(error.message, "VM Exception while processing transaction: revert");
-                }
-            });
+        it("allows both signers to withdraw in the other order", async function() {
+            const amount_withdrawn = 1;
 
-            it("allows one of the signers to propose but does not yet close", async function() {
-                await fundr.Close(destination_address, {from: signer1_address})
-                const accept = await fundr.accept.call();
-                assert(accept);
-            });
+            const balance_before = await web3.eth.getBalance(fundr.address);
+            const dest_balance_before = await web3.eth.getBalance(destination_address);
 
-            it("closes after both signers propose", async function() {
-                // Add ETH to fundr.address
-                const contract_balance_before = await web3.eth.getBalance(fundr.address);
-                const dest_balance_before = await web3.eth.getBalance(destination_address);
+            await fundr.Withdraw(destination_address, amount_withdrawn, {from: signer2_address});
+            await fundr.Withdraw(destination_address, amount_withdrawn, {from: signer1_address});
 
-                await fundr.Close(destination_address, {from: signer1_address});
-                await fundr.Close(destination_address, {from: signer2_address});
+            const balance_after = await web3.eth.getBalance(fundr.address);
+            const dest_balance_after = await web3.eth.getBalance(destination_address);
 
-                const accept = await fundr.accept.call();
-                assert(!accept);
-
-                const dest_balance_after = await web3.eth.getBalance(destination_address);
-                assert.equal(await web3.eth.getBalance(fundr.address), 0);
-                // plus and equals methods are because these are BigNumbers
-                assert(dest_balance_before.plus(contract_balance_before).equals(dest_balance_after));
-            });
-
-            it("closes after both signers sign in the other order", async function() {
-                // Add ETH to fundr.address
-                const contract_balance_before = await web3.eth.getBalance(fundr.address)
-                const dest_balance_before = await web3.eth.getBalance(destination_address)
-
-                await fundr.Close(destination_address, {from: signer2_address})
-                await fundr.Close(destination_address, {from: signer1_address})
-
-                const accept = await fundr.accept.call();
-                assert(!accept);
-
-                const dest_balance_after = await web3.eth.getBalance(destination_address)
-                assert.equal(await web3.eth.getBalance(fundr.address), 0)
-                // plus and equals methods are because these are BigNumbers
-                assert(dest_balance_before.plus(contract_balance_before).equals(dest_balance_after))
-            });
+            // Then make sure that amount was withdrawn
+            // plus and equals methods are because these are BigNumbers
+            assert(balance_after.plus(amount_withdrawn).equals(balance_before));
+            assert(dest_balance_before.plus(amount_withdrawn).equals(dest_balance_after));
         });
 
+        it("does not allow a non-signer to propose withdrawal", async function() {
+            try {
+                await fundr.Withdraw(destination_address, 1, {from: contributer_address})
+                assert.fail(null, null, "Contract should have raised.");
+            } catch(error) {
+                assert.equal(error.message, "VM Exception while processing transaction: revert");
+            }
+        });
+
+        it("does not allow the signers to withdraw more than is in the account", async function() {
+            const contract_balance_before = await web3.eth.getBalance(fundr.address)
+            // plus and equals methods are because these are BigNumbers
+            const withdraw_amount = contract_balance_before.plus(1)
+            try {
+                await fundr.Withdraw(destination_address, withdraw_amount, {from: signer1_address})
+                assert.fail(null, null, "Contract should have raised.");
+            } catch(error) {
+                assert.equal(error.message, "VM Exception while processing transaction: revert");
+            }
+        });
     });
 });
