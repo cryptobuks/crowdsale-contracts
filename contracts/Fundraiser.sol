4691bca2bb1d49eaf7546a33c1b7af2beb4e06d9
Alex Altair <alexander.altair@gmail.com>
2018-02-15 21:25:43 -0800

    Make wallet into 2 of 4 multi-sig!

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,66 +1,131 @@
 pragma solidity 0.4.18;
 
 contract Fundraiser {
-
   /* State */
-  address signer1;
-  address signer2;
-
   struct Proposal {
-    bool signed;
-    address destination;
     uint256 amount;
+    address signer;
+    address destination;
+    bool signed;
   }
-  
-  Proposal signer1_proposal;
-  Proposal signer2_proposal;
+
+  address[4] signers;
+  mapping(address => Proposal) signer_proposals;
 
   /* Constructor, choose signers. Those cannot be changed */
-  function Fundraiser(address init_signer1,
-                      address init_signer2) public {
-    signer1 = init_signer1;
-    signer2 = init_signer2;
-    signer1_proposal.signed = false;
-    signer2_proposal.signed = false;
+  function Fundraiser(
+    address init_signer0,
+    address init_signer1,
+    address init_signer2,
+    address init_signer3
+  ) public {
+    signers = [init_signer0, init_signer1, init_signer2, init_signer3];
+
+    signer_proposals[init_signer0] = Proposal({
+      amount: 0,
+      signer: init_signer0,
+      destination: 0x0,
+      signed: false
+    });
+
+    signer_proposals[init_signer1] = Proposal({
+      amount: 0,
+      signer: init_signer1,
+      destination: 0x0,
+      signed: false
+    });
+
+    signer_proposals[init_signer2] = Proposal({
+      amount: 0,
+      signer: init_signer2,
+      destination: 0x0,
+      signed: false
+    });
+
+    signer_proposals[init_signer3] = Proposal({
+      amount: 0,
+      signer: init_signer3,
+      destination: 0x0,
+      signed: false
+    });
   }
 
-  /* Entry point for contributors */
+  // TODO: what about indexed?
   event LogDeposit(address receiving_address, uint amount);
 
+  /* Entry point for contributors */
   function contribute(address receiving_address) external payable {
     LogDeposit(receiving_address, msg.value);
   }
 
   /* Entry points for signers */
-  function withdraw(address proposed_destination,
-                    uint256 proposed_amount) external {
+  function withdraw(
+    address proposed_destination,
+    uint256 proposed_amount
+  ) external {
     /* check that only one of the signers is requesting */
-    require(msg.sender == signer1 || msg.sender == signer2);
+    require(signer_proposals[msg.sender].signer == msg.sender);
     /* check amount */
     require(proposed_amount <= this.balance);
 
-    /* update action */
-    if (msg.sender == signer1) {
-      signer1_proposal.signed = true;
-      signer1_proposal.destination = proposed_destination;
-      signer1_proposal.amount = proposed_amount;
-    } else if (msg.sender == signer2) {
-      signer2_proposal.signed = true;
-      signer2_proposal.destination = proposed_destination;
-      signer2_proposal.amount = proposed_amount;
-    } else { assert(false); }
-    /* perform action */
+    update_proposal(proposed_destination, proposed_amount);
     maybe_perform_withdraw();
   }
 
+  function update_proposal(
+    address proposed_destination,
+    uint256 proposed_amount
+  ) internal {
+    signer_proposals[msg.sender].amount = proposed_amount;
+    signer_proposals[msg.sender].destination = proposed_destination;
+    signer_proposals[msg.sender].signed = true;
+  }
+
   function maybe_perform_withdraw() internal {
-    if (signer1_proposal.signed == true
-        && signer2_proposal.signed == true
-        && signer1_proposal.amount == signer2_proposal.amount
-        && signer1_proposal.destination == signer2_proposal.destination) {
-      signer1_proposal.signed = false;
-      signer2_proposal.signed = false;
-      signer1_proposal.destination.transfer(signer1_proposal.amount);
+    bool two_signers;
+    address first_signer;
+    address second_signer;
+
+    if (signed(0) && signed(1)) {
+      two_signers = true;
+      first_signer = signers[0];
+      second_signer = signers[1];
+    } else if (signed(0) && signed(2)) {
+      two_signers = true;
+      first_signer = signers[0];
+      second_signer = signers[2];
+    } else if (signed(0) && signed(3)) {
+      two_signers = true;
+      first_signer = signers[0];
+      second_signer = signers[3];
+    } else if (signed(1) && signed(2)) {
+      two_signers = true;
+      first_signer = signers[1];
+      second_signer = signers[2];
+    } else if (signed(1) && signed(3)) {
+      two_signers = true;
+      first_signer = signers[1];
+      second_signer = signers[3];
+    } else if (signed(2) && signed(3)) {
+      two_signers = true;
+      first_signer = signers[2];
+      second_signer = signers[3];
     }
+
+    if (two_signers) {
+      require(signer_proposals[first_signer].amount == signer_proposals[second_signer].amount);
+      require(signer_proposals[first_signer].destination == signer_proposals[second_signer].destination);
+
+      signer_proposals[signers[0]].signed = false;
+      signer_proposals[signers[1]].signed = false;
+      signer_proposals[signers[2]].signed = false;
+      signer_proposals[signers[3]].signed = false;
+
+      signer_proposals[first_signer].destination.transfer(signer_proposals[first_signer].amount);
+    }
+  }
+
+  function signed(uint index) internal view returns(bool) {
+    return signer_proposals[signers[index]].signed;
   }
 }
--- test/fundraiser.js
+++ test/fundraiser.js
@@ -1,28 +1,61 @@
 var Fundraiser = artifacts.require("Fundraiser")
 
+async function testWithdraw(signer1, signer2) {
+    amount_withdrawn = 1
+
+    balance_before = await web3.eth.getBalance(fundr.address)
+    dest_balance_before = await web3.eth.getBalance(destination_address)
+
+    await fundr.withdraw(destination_address, amount_withdrawn, {from: signer1})
+    await fundr.withdraw(destination_address, amount_withdrawn, {from: signer2})
+
+    balance_after = await web3.eth.getBalance(fundr.address)
+    dest_balance_after = await web3.eth.getBalance(destination_address)
+
+    // Then make sure that amount was withdrawn
+    // plus and equals methods are because these are BigNumbers
+    assert(balance_after.plus(amount_withdrawn).equals(balance_before), "Amount was not withdrawn!")
+    assert(dest_balance_before.plus(amount_withdrawn).equals(dest_balance_after), "Amount was not deposited!")
+}
+
 contract('Fundraiser', function(accounts) {
-    let fundr
-    let signer1_address
-    let signer2_address
-    let contributer_address
-    let destination_address
+    let all_signer_pairs = [
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [1, 0],
+        [1, 2],
+        [1, 3],
+        [2, 0],
+        [2, 1],
+        [2, 3],
+        [3, 0],
+        [3, 1],
+        [3, 2],
+    ]
 
     beforeEach(async function () {
-        signer1_address = accounts[1]
-        signer2_address = accounts[2]
-        contributer_address = accounts[3]
-        destination_address = accounts[4]
+        signers = [
+            accounts[1],
+            accounts[2],
+            accounts[3],
+            accounts[4],
+        ]
 
-        fundr = await Fundraiser.new(signer1_address, signer2_address)
+        fundr = await Fundraiser.new(...signers)
+
+        contributer_address = accounts[5]
+        destination_address = accounts[6]
     })
 
     it("reverts when ether is sent to it with no data", async function() {
         try {
             await fundr.sendTransaction({from: contributer_address, value: 1})
-            assert.fail(null, null, "Contract should have raised.")
         } catch(error) {
             assert.equal(error.message, "VM Exception while processing transaction: revert")
+            return
         }
+        assert.fail(null, null, "Contract should have raised.")
     })
 
     describe("when trying to contribute", function() {
@@ -32,7 +65,7 @@ contract('Fundraiser', function(accounts) {
 
             await fundr.contribute(
                 "0x0000000000000000000000000000000000000000",
-                {from: signer1_address, value: amount_sent}
+                {from: signers[0], value: amount_sent}
             )
 
             const balance_after = await web3.eth.getBalance(fundr.address)
@@ -54,23 +87,26 @@ contract('Fundraiser', function(accounts) {
 
         // Skipping because the contract abstraction seems to pad the address with zeros, making it valid.
         // We should figure out a way to send directly to the contract like a wallet would.
-        it.skip("revert if address is wrong", async function() {
+        it.skip("revert if address is malformed", async function() {
             try {
                 await fundr.contribute(
                     "0x1234",
                     {from: contributer_address}
                 )
-                assert.fail(null, null, "Contract should have raised.")
             } catch(error) {
                 assert.equal(error.message, "Invalid array length")
+                return
             }
+            assert.fail(null, null, "Contract should have raised.")
         })
+
+        it.skip("emits a LogDeposit event on success")
+        it.skip("does not emit a LogDeposit event on failure")
     })
 
     describe("when trying to withdraw", function() {
         beforeEach(async function () {
             await fundr.contribute(
-                // This hex is the concatenation of a fake public key and its double hash.
                 "0x0000000000000000000000000000000000000000",
                 {from: contributer_address, value: 1}
             )
@@ -78,55 +114,37 @@ contract('Fundraiser', function(accounts) {
 
         it("allows one signer to propose but not withdraw", async function() {
             const balance_before = await web3.eth.getBalance(fundr.address)
-            await fundr.withdraw(destination_address, 1, {from: signer1_address})
+            await fundr.withdraw(destination_address, 1, {from: signers[0]})
             const balance_after = await web3.eth.getBalance(fundr.address)
 
             assert(balance_after.equals(balance_before))
         })
 
-        it("allows both signers to withdraw", async function() {
-            const amount_withdrawn = 1
+        for (let pair of all_signer_pairs) {
+            it("allows all possible signer pairs to withdraw", async function() {
+                testWithdraw(signers[pair[0]], signers[pair[1]])
+            })
+        }
 
-            const balance_before = await web3.eth.getBalance(fundr.address)
-            const dest_balance_before = await web3.eth.getBalance(destination_address)
+        it("allows withdrawl twice", async function() {
+            await testWithdraw(signers[0], signers[1])
 
-            await fundr.withdraw(destination_address, amount_withdrawn, {from: signer1_address})
-            await fundr.withdraw(destination_address, amount_withdrawn, {from: signer2_address})
+            await fundr.contribute(
+                "0x0000000000000000000000000000000000000000",
+                {from: contributer_address, value: 1}
+            )
 
-            const balance_after = await web3.eth.getBalance(fundr.address)
-            const dest_balance_after = await web3.eth.getBalance(destination_address)
-
-            // Then make sure that amount was withdrawn
-            // plus and equals methods are because these are BigNumbers
-            assert(balance_after.plus(amount_withdrawn).equals(balance_before))
-            assert(dest_balance_before.plus(amount_withdrawn).equals(dest_balance_after))
-        })
-
-        it("allows both signers to withdraw in the other order", async function() {
-            const amount_withdrawn = 1
-
-            const balance_before = await web3.eth.getBalance(fundr.address)
-            const dest_balance_before = await web3.eth.getBalance(destination_address)
-
-            await fundr.withdraw(destination_address, amount_withdrawn, {from: signer2_address})
-            await fundr.withdraw(destination_address, amount_withdrawn, {from: signer1_address})
-
-            const balance_after = await web3.eth.getBalance(fundr.address)
-            const dest_balance_after = await web3.eth.getBalance(destination_address)
-
-            // Then make sure that amount was withdrawn
-            // plus and equals methods are because these are BigNumbers
-            assert(balance_after.plus(amount_withdrawn).equals(balance_before))
-            assert(dest_balance_before.plus(amount_withdrawn).equals(dest_balance_after))
+            await testWithdraw(signers[1], signers[0])
         })
 
         it("does not allow a non-signer to propose withdrawal", async function() {
             try {
                 await fundr.withdraw(destination_address, 1, {from: contributer_address})
-                assert.fail(null, null, "Contract should have raised.")
             } catch(error) {
                 assert.equal(error.message, "VM Exception while processing transaction: revert")
+                return
             }
+            assert.fail(null, null, "Contract should have raised.")
         })
 
         it("does not allow the signers to withdraw more than is in the account", async function() {
@@ -134,11 +152,12 @@ contract('Fundraiser', function(accounts) {
             // plus and equals methods are because these are BigNumbers
             const withdraw_amount = contract_balance_before.plus(1)
             try {
-                await fundr.withdraw(destination_address, withdraw_amount, {from: signer1_address})
-                assert.fail(null, null, "Contract should have raised.")
+                await fundr.withdraw(destination_address, withdraw_amount, {from: signers[0]})
             } catch(error) {
                 assert.equal(error.message, "VM Exception while processing transaction: revert")
+                return
             }
+            assert.fail(null, null, "Contract should have raised.")
         })
     })
 })
